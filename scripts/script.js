faf0ac5011285e4205f1c150fadaca5f8952c0fd
gumiz <micgrn@gmail.com>
2016-02-19 14:47:12 +0100

    actor picker

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
-var audioElement;
+//https://plnkr.co/edit/uatHxrMwe78zTNkMJAtI?p=preview
+
+var _counterLengthInSeconds = 3;
+var audioHit;
+var audioCountdown;
+var audioWin;
 var freezeTimer = true;
 var timer = 0;
 var items = ["item5", "item4", "item3", "item2", "item1"];
@@ -33,26 +38,59 @@ var initElement = function(id) {
     };
 };
 
-function playSound() {
-    audioElement.play();
+function playHit() {
+    audioHit.play();
+}
+
+function playCountdown() {
+    audioCountdown.play();
+}
+
+function playWin() {
+    audioWin.play();
 }
 
 function removeItem() {
     var item = items.pop();
     var obj = document.getElementById(item);
     obj.style.visibility = "hidden";
-    if (items.length == 0)
+    if (items.length == 0) {
+        playWin();
         stopTimer();
+    }
 }
 
-function setItemVisible(itemId) {
-    document.getElementById(itemId).style.visibility = "visible";
+function resetActor(itemId) {
+    debugger;
+    var style = $( "#actorPicker option:selected" ).text();
+    var item = document.getElementById(itemId);
+    item.style.visibility = "visible";
+    item.className = '';
+    item.className = style;
+}
+
+function resetActors() {
+    resetActor('item1');
+    resetActor('item2');
+    resetActor('item3');
+    resetActor('item4');
+    resetActor('item5');
 }
 
 window.onload = function() {
-    audioElement = document.createElement('audio');
-    audioElement.setAttribute('src', 'audio/audio.mp3');
+    hideResults();
+    document.getElementById('actorPicker').onchange=function(){
+        resetActors();
+    };
+    audioHit = document.createElement('audio');
+    audioHit.setAttribute('src', 'audio/hit.mp3');
+    audioCountdown = document.createElement('audio');
+    audioCountdown.setAttribute('src', 'audio/countdown.mp3');
+    audioWin = document.createElement('audio');
+    audioWin.setAttribute('src', 'audio/win.mp3');
 
+    initElement('timer');
+    initElement('legend');
     initElement('item1');
     initElement('item2');
     initElement('item3');
@@ -67,7 +105,6 @@ window.onload = function() {
         var d = new Date();
         var currentTime = d.getTime();
         var clock = document.getElementById("clock");
-        debugger;
         var value = (Number(currentTime) - Number(timer)) / 1000;
         var result = value.toFixed(1);
         clock.innerHTML = result;
@@ -76,37 +113,61 @@ window.onload = function() {
 };
 
 function restartGame() {
+    hideResults();
+    restartTimer();
+    playCountdown();
     items = ["item5", "item4", "item3", "item2", "item1"];
-    setItemVisible('item1');
-    setItemVisible('item2');
-    setItemVisible('item3');
-    setItemVisible('item4');
-    setItemVisible('item5');
-    startTimer();
+    resetActors();
+    setTimeout(function(){
+        startTimer();
+    }, 1000*_counterLengthInSeconds);
 }
 
 function startTimer() {
+    hideResults();
     freezeTimer = false;
     var d = new Date();
     timer = d.getTime();
-    document.getElementById("clock").innerHtml = "0.0";
+    document.getElementById("clock").innerHTML = "0.0";
     document.getElementById("clock").style.visibility = "visible";
 }
 function stopTimer() {
     freezeTimer = true;
+    showResults();
 }
+
+function timerIsRunning() {
+    return !freezeTimer;
+}
+
 function restartTimer() {
-    document.getElementById("clock").innerHtml = "0.0";
+    debugger;
+    freezeTimer = true;
+    document.getElementById("clock").innerHTML = "0.0";
+}
+
+function showResults() {
+    document.getElementById("clockBig").innerHTML = document.getElementById("clock").innerHTML;
+    document.getElementById("clock").style.visibility = "hidden";
+    document.getElementById("clockBig").style.visibility = "visible";
+}
+
+function hideResults() {
+    document.getElementById("clock").style.visibility = "visible";
+    document.getElementById("clockBig").style.visibility = "hidden";
 }
 
 $(window).keypress(function (e) {
     if (e.keyCode === 0 || e.keyCode === 32) {
         e.preventDefault();
         removeItem();
-        playSound();
+        playHit();
     }
     if(e.which == 13) {
-        restartGame();
+        if (timerIsRunning())
+            stopTimer();
+        else
+            restartGame();
     }
 });
 
